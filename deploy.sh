67d93a1414504cebafa55745ff9321b87d2aca03
Peter <davison.pl@gmail.com>
2022-12-05 22:19:07 -0500

    Fix deploy script to avoid uploading venv files

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-aws s3 sync . s3://bowl-pickem-public --exclude="*" --include="*.html" --include="*.css" --include="*.js"
+aws s3 sync . s3://bowl-pickem-public --exclude="*" --include="*.html" --include="*.css" --include="*.js" --exclude="venv/*"
 aws cloudfront create-invalidation --distribution-id E25KXPCJUCQE6E --paths "/*"
